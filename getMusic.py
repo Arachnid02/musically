b813db4a56e9aa580945d1cb509d1a16e7e6cd9d
danielgermany <daniel.germany@uamaker.nyc>
2019-05-21 12:27:51 -0400

    Bare bones for getting music from youtube ( can be edited to get mp3 files from any site)

---
+++ getMusic.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+import youtube_dl
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+    'logger': MyLogger(),
+    'progress_hooks': [my_hook],
+}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.youtube.com/watch?v=BaW_jenozKc'])
