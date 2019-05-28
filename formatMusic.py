6a575234d7adaa523c4dbb123a71cd2eab2c60bb
danielgermany <daniel.germany@uamaker.nyc>
2019-05-28 12:50:14 -0400

    Code formats the .tsv file into a usable tuple

---
+++ formatMusic.py
@@ -0,0 +1,9 @@
+songLib = open("songLib.tsv ","r")
+songLib_data = []
+for line in songLib:
+    number_songs = line.split("\t")
+    songs = [n for n in number_songs]
+    songLib_data.append(songs)
+
+songLib_data_tuple = tuple(songLib_data)
+print(songLib_data_tuple[4][2])
