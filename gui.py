62c7bc1e062bb6eab9c7cb49b2d5970b8f67c7c0
Kaylee Nieves <kayleenieves@Kaylees-MacBook-Pro.local>
2019-06-12 09:36:23 -0400

    intro screen created without functions

--- gui.py
+++ gui.py
@@ -5,26 +5,31 @@ musicWin = GraphWin("Musically", 700, 700)
 musicWin.setCoords(0,0,700,700)
 
 #Text
-intro = Text(Point(350, 500), "Welcome to our Music Program!")
+intro = Text(Point(350, 570), "Welcome to our Music Program!")
 intro.setSize(20)
 intro.setStyle("bold")
 intro.setTextColor("black")
 intro.draw(musicWin)
 
-direct = Text(Point(350, 450), "We are gonna ask you a couple questions about YOUR favorite song!")
+direct = Text(Point(350, 520), "We are gonna ask you a couple questions about YOUR favorite song!")
 direct.setSize(20)
 direct.setStyle("bold")
 direct.setTextColor("black")
 direct.draw(musicWin)
 
 #music note image
-#mnoteImage = Image(Point(350,650), "music notes.png")
-#mnoteImage.draw(musicWin)
+mnoteImage = Image(Point(350,210), "music notes.png")
+mnoteImage.draw(musicWin)
 
 #next button
-nextRec = Rectangle(Point(500,100),Point(650,200))
+nextRec = Rectangle(Point(610,10),Point(690,60))
 nextRec.setFill(color_rgb(0,255,0))
 nextRec.draw(musicWin)
 
+nextTex = Text(Point(650, 35), "next page")
+nextTex.setSize(15)
+nextTex.setStyle("bold")
+nextTex.setTextColor("black")
+nextTex.draw(musicWin)
 
               
